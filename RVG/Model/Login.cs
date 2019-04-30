6430bad0663bd8e073827a56f57d40b5e6991d54
Jonas Andersen <jona32d8@edu.easj.dk>
2019-04-30 12:22:37 +0200

    Updated login

---
+++ RVG/Model/Login.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RVG.Model
+{
+    public class Login
+    {
+        private string _password = "1234";
+        private string _input;
+
+        public string Password
+        {
+            get { return _password;}
+            set { _password = value; }
+        }
+
+
+        public bool PasswordCheck(string tekst)
+        {
+            if (Password == tekst)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ RVG/ViewModel/LoginViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using RVG.Common;
+using RVG.Model;
+
+namespace RVG.ViewModel
+{
+    public class LoginViewModel
+    {
+        private Login _login;
+
+        public LoginViewModel()
+        {
+            _login = new Login();
+
+            //CheckCommand = new RelayCommand(CheckMethod);
+        }
+
+        public ICommand CheckCommand { get; set; }
+
+        //public string SelectedInput { get { return  } }
+
+        //public void CheckMethod()
+        //{
+        //    if (_login.PasswordCheck())
+        //    {
+        //        //reee//
+        //    }
+        //    else
+        //    {
+
+        //    }
+        }
+    }
+}
