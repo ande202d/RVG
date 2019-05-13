156d0b0c51850518f2a04daeb69749f8903b26fd
Jacob Berglund Madvig <jaco6002@edu.easj.dk>
2019-05-13 10:53:34 +0200

    oprettet startpage og blank PersonalePage

--- RVG/App.xaml.cs
+++ RVG/App.xaml.cs
@@ -67,7 +67,7 @@ namespace RVG
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(login), e.Arguments);
+                    rootFrame.Navigate(typeof(StartPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
