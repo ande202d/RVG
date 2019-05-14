126f8ecb86d141ab08a6f0b4372d855d9cb440c2
Timm Oleander Nielsen <timm0192@edu.easj.dk>
2019-05-14 13:24:20 +0200

    Update(Done) men ikke DONE DONE og Delete(DONE DONE) artefact

--- RVG/Model/ArtefactCatalogSingleton.cs
+++ RVG/Model/ArtefactCatalogSingleton.cs
@@ -40,6 +40,11 @@ namespace RVG.Model
             _artefactslist.Add(a);
         }
 
+        public void UpdateArtefact(Artefacts a)
+        {
+            _artefactslist[_artefactslist.IndexOf(a)] = a;
+        }
+
         public void RemoveArtefact(Artefacts a)
         {
             _artefactslist.Remove(a);
