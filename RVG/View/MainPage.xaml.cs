5c8347eb8b4ce27d8cd7b7191a349d4bdba10958
Carl Guarin Dreslet <carl0572@edu.easj.dk>
2019-05-16 11:34:30 +0200

    Ryddet op i cs Mainpage

--- RVG/View/MainPage.xaml.cs
+++ RVG/View/MainPage.xaml.cs
@@ -18,6 +18,7 @@ using RVG.ViewModel;
 using Windows.Media.Playback;
 using Windows.Media.Core;
 using RVG.Model;
+using RVG.View;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -28,50 +29,20 @@ namespace RVG
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        //private ArtefactCatalogSingleton ACS;
-        //private CatalogViewModelSingleton CVMS;
-        private static int _buttonIdCounter = 1;
-        private int _buttonId;
+        
         public MainPage()
         {
             this.InitializeComponent();
-            //ACS = ArtefactCatalogSingleton.Instance;
-            //CVMS = new CatalogViewModelSingleton();
-            //CVMS.Iniliatisere();
             
-            //UpdateButtons();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Button b = e.OriginalSource as Button;
 
-            
-            //CatalogViewModel.StaticSelectedArtefacts = ACS.GetArtefacts.Find(a => b.Name.Equals(a.ArtefactID.ToString()));
-            //CatalogViewModel.LoadMethod(ACS.GetArtefacts.Find(a => b.Name.Equals(a.ArtefactID.ToString())));
-            //CVMS.LoadMethod(CVMS.CatalogSingleton.GetArtefacts.Find(a => b.Name.Equals(a.ArtefactID.ToString())));
+
+            Frame.Navigate(typeof(StartPage));
             
         }
 
-        public void UpdateButtons()
-        {
-            //Canvas c = new Canvas();
-            //foreach (Artefacts a in CVMS.CatalogSingleton.GetArtefacts)
-            //{
-            //    Button b = new Button();
-            //    b.Style = (Style)this.Resources["ArtefactButton"];
-            //    //b.Margin = new Thickness{Left = a.Xpos, Top = a.Ypos};
-            //    Canvas.SetLeft(b, a.Xpos + 50);
-            //    Canvas.SetTop(b, a.Ypos);
-            //    b.Content = $"{a.ArtefactID}xx";
-            //    b.Name = a.ArtefactID.ToString();
-
-            //    b.Click += Button_Click;
-            //    //b.Click += new RoutedEventHandler();
-
-            //    c.Children.Add(b);
-            //}
-            //StackPanelKort.Children.Add(c);
-        }
     }
 }
