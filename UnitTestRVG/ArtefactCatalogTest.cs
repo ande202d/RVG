9e1df2e70367ab8976dd69cb42d50db45f1f2490
Anders Garbacz Hansen <ande202d@edu.easj.dk>
2019-05-23 09:18:42 +0200

    added ArtefactCatalogTest

---
+++ UnitTestRVG/ArtefactCatalogTest.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace UnitTestRVG
+{
+    [TestClass]
+    public class ArtefactCatalogTest
+    {
+        private void CreateObjects()
+        {
+
+        }
+
+        [TestMethod]
+        public void AddArtefactTest_Initialized()
+        {
+
+        }
+
+        [TestMethod]
+        public void AddArtefactTest_UnInitialized()
+        {
+
+        }
+
+    }
+}
